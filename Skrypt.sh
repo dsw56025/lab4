20b7291d9dc47a7ae4835e9968a9531747eaf11c
Krzysztof <dsw56025@student.dsw.edu.pl>
2025-06-14 10:37:37 +0200

    dodano polecenie --init

--- Skrypt.sh
+++ Skrypt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$1" == "--date" || "$1" == "-d" ]; then
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
     date
-elif [ "$1" == "--logs" || "$1" == "-l" ]; then
+elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
 	amount=${2:-100}
     for ((i=1; i<=amount; i++)); do
         logfile="log${i}.txt"
@@ -10,9 +10,15 @@ elif [ "$1" == "--logs" || "$1" == "-l" ]; then
         echo "Skrypt: $0" >> "$logfile"
         echo "Data: $(date)" >> "$logfile"
     done
-elif [ "$1" == "--help" || "$1" == "-h" ]; then
+elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
     echo "Polecenia:"
     echo "  --date           Wyświetla dzisiejszą datę"
     echo "  --logs [liczba]  Tworzy pliki log.txt (domyslnie 100) "
     echo "  --help           Wyświetla pomoc"
+	
+elif [[ "$1" == "--init" ]]; then
+
+	git clone "https://github.com/dsw56025/lab4.git"
+    export PATH="$PATH:$(pwd)"
+
 fi

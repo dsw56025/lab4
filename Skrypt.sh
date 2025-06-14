050de49fd155698a24d66c2bf594fe989cfac302
Krzysztof <dsw56025@student.dsw.edu.pl>
2025-06-14 09:28:32 +0200

    komenda --logs

--- Skrypt.sh
+++ Skrypt.sh
@@ -2,4 +2,11 @@
 
 if [ "$1" == "--date" ]; then
     date
+elif [ "$1" == "--logs" ]; then
+    for i in {1..100}; do
+        logfile="log${i}.txt"
+        echo "Plik: $logfile" > "$logfile"
+        echo "Skrypt: $0" >> "$logfile"
+        echo "Data: $(date)" >> "$logfile"
+    done
 fi
